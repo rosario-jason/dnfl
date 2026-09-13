a585e338aedac1baf59f6d60412a7871b806ab0f
Jason Rosario <rosario.jason@gmail.com>
2026-09-13 15:17:41 -0400

    Header v2.01 Update

--- scripts/dnfl-header-script-v2.js
+++ scripts/dnfl-header-script-v2.js
@@ -9,7 +9,7 @@
     window.DNFL = window.DNFL || {};
 
     // 2. FRAMEWORK VERSION (Increment this to bust browser cache globally)
-    const FRAMEWORK_VERSION = "2.00";
+    const FRAMEWORK_VERSION = "2.01";
     const BASE_URL = "https://dnfl.live/scripts/";
 
     /* 3. MASTER CHRONOLOGICAL SCRIPT REGISTRY
