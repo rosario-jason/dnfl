c1405041d8a5c0c16c5d1e3697503165adfe19cb
Jason Rosario <rosario.jason@gmail.com>
2026-09-20 16:49:12 -0400

    DNFL Standings TEST4

--- scripts/dnfl-header-TEST.js
+++ scripts/dnfl-header-TEST.js
@@ -12,7 +12,7 @@
     const LOG_PREFIX = `[DNFL Framework]`;
 
     const CONFIG = {
-        VERSION: "3.14-TEST",
+        VERSION: "3.15-TEST",
         BASE_URL: "https://dnfl.live/scripts/",
 
         STYLESHEETS: [
@@ -26,7 +26,7 @@
         ],
 
         FEATURE_MODULES: [
-            { name: "standings", url: "dnfl-standings-TEST3.js" },
+            { name: "standings", url: "dnfl-standings-TEST4.js" },
             { name: "rankings",  url: "dnfl-rankings.js" },
             { name: "podcast",   url: "dnfl-podcast-TEST.js" },
             { name: "rules",     url: "dnfl-rules.js" }
