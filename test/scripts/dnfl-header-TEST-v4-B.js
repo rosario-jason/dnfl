023b445ca711ff45978edf7821a2881da9752a85
Jason Rosario <rosario.jason@gmail.com>
2026-09-25 18:35:31 -0400

    DNFL Test v4.58

--- test/scripts/dnfl-header-TEST-v4-B.js
+++ test/scripts/dnfl-header-TEST-v4-B.js
@@ -10,11 +10,11 @@
     // 1. Expanded Framework Configuration
     // =========================================================================
     const CONFIG = {
-        VERSION: "4.57-B",
+        VERSION: "4.58-B",
         BASE_URL: "https://dnfl.live/test/scripts/",
 
         STYLESHEETS: [
-            { id: "dnfl-global-css", url: "https://dnfl.live/test/css/dnfl-global-v4_47.css" }
+            { id: "dnfl-global-css", url: "https://dnfl.live/test/css/dnfl-global-v4_48.css" }
         ],
 
         INFRASTRUCTURE: [
